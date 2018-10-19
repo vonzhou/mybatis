cd1417cb2bdd8c862043d091d6af778b84e57cf7
vonzhou <vonzhou@163.com>
2018-10-19 16:00:07 +0800

    KeyGenerator

--- src/main/java/org/apache/ibatis/executor/keygen/KeyGenerator.java
+++ src/main/java/org/apache/ibatis/executor/keygen/KeyGenerator.java
@@ -23,15 +23,15 @@ import org.apache.ibatis.mapping.MappedStatement;
 /**
  * @author Clinton Begin
  */
+
 /**
  * 键值生成器
- *
  */
 public interface KeyGenerator {
 
-  //定了2个回调方法，processBefore,processAfter
-  void processBefore(Executor executor, MappedStatement ms, Statement stmt, Object parameter);
+    //定了2个回调方法，processBefore,processAfter
+    void processBefore(Executor executor, MappedStatement ms, Statement stmt, Object parameter);
 
-  void processAfter(Executor executor, MappedStatement ms, Statement stmt, Object parameter);
+    void processAfter(Executor executor, MappedStatement ms, Statement stmt, Object parameter);
 
 }
